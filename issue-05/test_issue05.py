aab51806b007876d2b8c7221cb9229ce0564ea23
zhiga <zhigarink@gmail.com>
2021-12-10 21:28:14 +0300

    mock fixes

--- issue-05/test_issue05.py
+++ issue-05/test_issue05.py
@@ -1,34 +1,35 @@
 from unittest.mock import patch
 from issue05 import what_is_year_now
 import pytest
-import json
+from urllib import request
+import io
 
 
 def test_what_is_year_now_exception():
-    with patch.object(json, 'loads',
-                      return_value={'currentDateTime': '2021:11:27T18:21Z'}):
+    data = io.StringIO('{"currentDateTime": "2021:11:27T18:21Z"}')
+    with patch.object(request, 'urlopen', return_value=data):
         with pytest.raises(ValueError):
             _ = what_is_year_now()
 
 
 def test_what_is_year_now_empty_year():
-    with patch.object(json, 'load',
-                      return_value={'currentDateTime': '?-11-27T18:21Z'}):
+    data = io.StringIO('{"currentDateTime": "?-11-27T18:21Z"}')
+    with patch.object(request, 'urlopen', return_value=data):
         with pytest.raises(ValueError):
             _ = what_is_year_now()
 
 
 def test_what_is_year_now_YMD():
     exp_year = 2021
-    with patch.object(json, 'load',
-                      return_value={'currentDateTime': '2021-11-27T18:21Z'}):
+    data = io.StringIO('{"currentDateTime": "2021-11-27T18:21Z"}')
+    with patch.object(request, 'urlopen', return_value=data):
         year = what_is_year_now()
     assert exp_year == year
 
 
 def test_what_is_year_now_DMY():
     exp_year = 2021
-    with patch.object(json, 'load',
-                      return_value={'currentDateTime': '01.03.2021T18:21Z'}):
+    data = io.StringIO('{"currentDateTime": "01.03.2021T18:21Z"}')
+    with patch.object(request, 'urlopen', return_value=data):
         year = what_is_year_now()
     assert exp_year == year
